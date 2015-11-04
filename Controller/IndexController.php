878caf3b6e8dc7779eca2d93a0aa60857d8a8daf
tema <artemkuchma@gmail.com>
2015-11-04 08:49:57 -0800

    Creating a  simple file sistem of my site

---
+++ Controller/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Art
+ * Date: 04.11.15
+ * Time: 8:30
+ * To change this template use File | Settings | File Templates.
+ */
+
+class IndexController {
+
+}
---
+++ Controller/MenuController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Art
+ * Date: 04.11.15
+ * Time: 8:46
+ * To change this template use File | Settings | File Templates.
+ */
+
+class MenuController {
+
+}
